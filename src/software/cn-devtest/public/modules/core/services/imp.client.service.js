4a6a80d304064fbae0159070224e0461afa30e91
bobbyziom <bobbyziom@gmail.com>
2015-05-09 16:53:01 +0200

    showcase

--- src/software/cn-devtest/public/modules/core/services/imp.client.service.js
+++ src/software/cn-devtest/public/modules/core/services/imp.client.service.js
@@ -19,7 +19,93 @@ angular.module('core').factory('Imp', [ '$http',
 
 		      $http(req).success(callback);
 
+		    },
+
+		    setKeen: function(impId, keenCollection, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/collection/' + keenCollection,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
+		    },
+
+		    setDeviceName: function(impId, deviceName, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/name/' + deviceName,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
+		    },
+
+		    addEmail: function(impId, email, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/notification/email/' + email,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
+		    },
+
+		    setBatteryTrigger: function(impId, value, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/notification/battery/' + value,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
+		    },
+
+		    setMoistureTrigger: function(impId, value, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/notification/moisture/' + value,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
+		    },
+
+		    setWakingInterval: function(impId, value, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/config/interval/' + value,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
+		    },
+
+		    setCollectAmount: function(impId, value, callback) {
+
+		      var req = {
+		        method: 'POST',
+		        url: agentUrl + impId + '/setup/config/collect/' + value,
+		        headers: {'Content-Type': 'application/json'}
+		      };
+
+		      $http(req).success(callback);
+
 		    }
+
+
 		};
 	}
 ]);
